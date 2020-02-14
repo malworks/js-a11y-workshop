44a81a19d85e20d33122920078ec19bac60f564e
Mallory Robertson <malloryworks@gmail.com>
2020-02-13 21:05:56 -0800

    Progressive enhancement section

--- src/components/better/tab-list.js
+++ src/components/better/tab-list.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
 const TabList = ({ items = [] }) => {
-  const [isClient, setClient] = useState(false)
-  /*
-   * Think of this as componentDidMount
-   * on the client, it will run once and update state
-   */
+  const [isClient, setClient] = useState(false) 
+
   useEffect(() => {
     setClient(true)
   }, [])
   return (
-    <></>
+    <ul role={ isClient ?  `tablist` : `list` }> {}
+      {items.map(item => (
+        <li key={item.id} {...isClient && { role: `tab` }}> {}
+          {item.label}
+        </li>
+      ))}
+    </ul>
   )
 }
 
